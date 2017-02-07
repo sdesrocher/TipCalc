4199f39ccee57c2e3c05c520c72826d9aeb2c3e9
sdesrocher <sdesrocher@css.edu>
2017-02-06 20:34:01 -0600

    radio buttons added

--- app/src/main/java/css/cis3334/tipcalc/MainActivity.java
+++ app/src/main/java/css/cis3334/tipcalc/MainActivity.java
@@ -1,19 +1,21 @@
 package css.cis3334.tipcalc;
 
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    //declare needed variables for widgets 
+    //declare needed variables for widgets
     EditText etBill;
-    EditText etNumberPeople;
     TextView tvTotalTip;
     TextView tvTipPerPerson;
     CheckBox cbService;
+    RadioButton rb1, rb2, rb3;
 
 
     @Override
@@ -22,11 +24,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         etBill = (EditText)findViewById(R.id.editTextBill);
-        etNumberPeople = (EditText) findViewById(R.id.editTextNumberPeople);
         tvTotalTip = (TextView) findViewById(R.id.textViewTotalTip);
         tvTipPerPerson = (TextView) findViewById(R.id.textViewTotalTip);
         cbService = (CheckBox) findViewById(R.id.checkBoxService);
-
+        rb1 = (RadioButton) findViewById(R.id.radioButton1);
+        rb2 = (RadioButton) findViewById(R.id.radioButton2);
+        rb3 = (RadioButton) findViewById(R.id.radioButton3);
 
     }
 
@@ -34,7 +37,17 @@ public class MainActivity extends AppCompatActivity {
     public void onClickButtonCalc(View view) {
         //comment to test drive Github
         Double bill = Double.parseDouble(etBill.getText().toString());
-        Double numPeople = Double.parseDouble(etNumberPeople.getText().toString());
+        int numPeople = 1;
+        if (rb1.isChecked()) {
+            numPeople = 1;
+        }
+        if (rb2.isChecked()) {
+            numPeople = 2;
+        }
+        if (rb3.isChecked()) {
+            numPeople = 3;
+        }
+
         Double totalTip;                //have to declare outside if-than
 
         if (cbService.isChecked()) {
